c21c36bd8204173d2beb3cfd2e5fb1d4672a4ff7
S1ye <54349457+S1ye@users.noreply.github.com>
2023-01-28 15:56:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 (async () =>
 {
-	document.addEventListener('copy', function(e)
-	{
-		var text = window.getSelection().toString();
-		e.clipboardData.setData('text/plain', text);
-		e.preventDefault();
-	});
-
-	window.extraemotes_urls = await GetEmotesUrls();
+	const EMOTES_URL = "https://s1ye.github.io/GGExtraEmotes/emotes/";
+	const extraemotes_urls = await GetEmotesUrls();
 	InfinityAddNickClickEvent();
 	InfinityAddCustomInput();
 	InfinityAddEmojis();
@@ -20,7 +14,10 @@
 
 	async function InfinityAddCustomInput()
 	{
-		await AddCustomInput();
+		if (!document.getElementById("ggextraemotes_msg_input"))
+		{
+			await AddCustomInput();
+		}
 		setTimeout(InfinityAddCustomInput, 500);
 	}
 
@@ -50,59 +47,12 @@
 
 	async function AddCustomInput()
 	{
-		if (document.getElementById("ggextraemotes_msg_input")) { return; }
-		var text_block = await GetElementByXPath('//div[@class="chat-control-block"]//div[@class="text-block ng-scope"]');
-		var textarea = await GetElementByXPath('//div[@class="chat-control-block"]//div[@class="textarea"]');
-		textarea.style = "display: none";
-		var ggextraemotes_msg_input = document.createElement("div");
-		ggextraemotes_msg_input.id = "ggextraemotes_msg_input";
-		ggextraemotes_msg_input.className = "textarea";
-		ggextraemotes_msg_input.setAttribute("contenteditable", true);
-		ggextraemotes_msg_input.setAttribute("placeholder", "Написать сообщение...");
-		ggextraemotes_msg_input.onkeyup = function(event)
-		{
-			if (event.keyCode != 13) { return; }
-			var msg = ggextraemotes_msg_input.innerHTML;
-			msg = Replace(msg, "<div>", "");
-			msg = Replace(msg, "</div>", "");
-			msg = Replace(msg, "<br>", "");
-			msg = Replace(msg, "&nbsp;", " ");
-			msg = Replace(msg, "\n", "");
-			msg = Replace(msg, "\r", "");
-			msg = Replace(msg, "\t", "");
-			for (var i = 0; i < window.extraemotes_urls.length; i++)
-			{
-				var replace = '<img class="ggextraemote_in_input" src="' + window.extraemotes_urls[i] + '">';
-				msg = Replace(msg, replace, ' ' + window.extraemotes_urls[i] + ' ');
-			}
-			var emotes = msg.split("<divforemote");
-			for (var i = 1; i < emotes.length; i++)
-			{
-				var inner_emote = emotes[i].split("</divforemote>")[0];
-				try
-				{
-					var name = inner_emote.split('tooltip="')[1].split('"')[0];
-					msg = Replace(msg, "<divforemote" + inner_emote + "</divforemote>", " " + name + " ");
-				}
-				catch(e)
-				{
-					msg = Replace(msg, "<divforemote" + inner_emote + "</divforemote>", "");
-				}
-			}
-			console.log("Отправлено: '" + msg + "'");
-			textarea.innerText = msg;
-			var event = new KeyboardEvent("keypress",
-			{
-				keyCode: 13,
-				bubbles: false,
-				cancelable: false
-			});
-			textarea.dispatchEvent(event);
-			ggextraemotes_msg_input.innerHTML = "";
-			setTimeout(function() { ggextraemotes_msg_input.innerHTML = "" }, 100);
-		}
+		const text_block = await GetElementByXPath('//div[@class="chat-control-block"]//div[@class="text-block ng-scope"]');
+		const source_textarea = await GetElementByXPath('//div[@class="chat-control-block"]//div[@class="textarea"]');
+		source_textarea.style = "display: none";
+		const ggextraemotes_msg_input = CreateGGExtraEmotesMsgInput();
 		text_block.prepend(ggextraemotes_msg_input);
-		var observer = new MutationObserver(function(mutations)
+		const observer = new MutationObserver(function(mutations)
 		{
 			for (var i = 0; i < mutations.length; i++)
 			{
@@ -110,28 +60,110 @@
 				smile_div.setAttribute("contenteditable", false);
 				var element = mutations[i].addedNodes[0];
 				if (!element) { continue; }
-				if (!IsElement(element)) { continue; }
+				if (!IsHTMLElement(element)) { continue; }
 				smile_div.append(element);
 				ggextraemotes_msg_input.append(smile_div);
 			}
 		});
-		observer.observe(textarea, { childList: true, subtree: true });
+		observer.observe(source_textarea, { childList: true, subtree: true });
+
+		ggextraemotes_msg_input.onpaste = function(clipboard_event)
+		{
+			return InsertTextOnly(clipboard_event);
+
+			function InsertTextOnly(clipboard_event)
+			{
+				clipboard_event.preventDefault();
+				const text = (clipboard_event.clipboardData || window.clipboardData).getData("text");
+				const selection = window.getSelection();
+				if (!selection.rangeCount) return;
+				selection.deleteFromDocument();
+				selection.getRangeAt(0).insertNode(document.createTextNode(text));
+				selection.collapseToEnd();
+				return false;
+			}
+		}
+		ggextraemotes_msg_input.onkeyup = function(keyboard_event)
+		{
+			if (keyboard_event.keyCode != 13) { return; }
+			keyboard_event.preventDefault();
+			const text = ReplaceHTMLWithText(ggextraemotes_msg_input.innerHTML);
+			SendMessage(text);
+			return false;
+
+			function ReplaceHTMLWithText(html)
+			{
+				var text = CopyStrByValue(html);
+				text = Replace(text, "))", " " + EMOTES_URL + "Brackets.png ");
+				text = Replace(text, "<div>", "");
+				text = Replace(text, "</div>", "");
+				text = Replace(text, "<br>", "");
+				text = Replace(text, "&nbsp;", " ");
+				text = Replace(text, "\n", "");
+				text = Replace(text, "\r", "");
+				text = Replace(text, "\t", "");
+				for (var i = 0; i < extraemotes_urls.length; i++)
+				{
+					const replace = '<img class="ggextraemote_in_input" src="' + extraemotes_urls[i] + '">';
+					text = Replace(text, replace, ' ' + extraemotes_urls[i] + ' ');
+				}
+				const emotes = text.split("<divforemote");
+				for (var i = 1; i < emotes.length; i++)
+				{
+					const inner_emote = emotes[i].split("</divforemote>")[0];
+					try
+					{
+						const name = inner_emote.split('tooltip="')[1].split('"')[0];
+						text = Replace(text, "<divforemote" + inner_emote + "</divforemote>", " " + name + " ");
+					}
+					catch(e)
+					{
+						text = Replace(text, "<divforemote" + inner_emote + "</divforemote>", "");
+					}
+				}
+				return text;
+			}
+
+			function SendMessage(text)
+			{
+				console.log("Отправка сообщения: '" + text + "'");
+				source_textarea.innerText = text;
+				source_textarea.dispatchEvent(new KeyboardEvent("keypress",
+				{
+					keyCode: 13,
+					bubbles: false,
+					cancelable: false
+				}));
+				ggextraemotes_msg_input.innerHTML = "";
+				setTimeout(function() { ggextraemotes_msg_input.innerHTML = "" }, 100);
+			}
+		}
+
+		function CreateGGExtraEmotesMsgInput()
+		{
+			const ggextraemotes_msg_input = document.createElement("div");
+			ggextraemotes_msg_input.id = "ggextraemotes_msg_input";
+			ggextraemotes_msg_input.className = "textarea";
+			ggextraemotes_msg_input.setAttribute("contenteditable", true);
+			ggextraemotes_msg_input.setAttribute("placeholder", "Написать сообщение...");
+			return ggextraemotes_msg_input;
+		}
 	}
 
 	async function AddEmojis()
 	{
 		var smile_list = await GetElementByXPath('//*[@id="smiles"]//div[@class="smile-list"]');
 		if (smile_list.getElementsByClassName("ggextraemote")[0]) { return; }
-		for (var i = 0; i < window.extraemotes_urls.length; i++)
+		for (var i = 0; i < extraemotes_urls.length; i++)
 		{
 			var emoji = document.createElement("img");
-			emoji.src = window.extraemotes_urls[i];
+			emoji.src = extraemotes_urls[i];
 			emoji.className = "ggextraemote";
 			emoji.onclick = function()
 			{
 				var custom_input = document.getElementById("ggextraemotes_msg_input");
 				custom_input.innerHTML += '<img class="ggextraemote_in_input" src="' + this + '">';
-			}.bind(window.extraemotes_urls[i]);
+			}.bind(extraemotes_urls[i]);
 			smile_list.prepend(emoji);
 		}
 	}
@@ -152,17 +184,20 @@
 
 	async function GetEmotesUrls()
 	{
-		var EMOTES_LIST_URL = "https://s1ye.github.io/GGExtraEmotes/list.txt";
-		var EMOTES_URL = "https://s1ye.github.io/GGExtraEmotes/emotes/";
 		return await new Promise(async function(resolve, reject)
 		{
 			async function InnerGetEmotesUrls()
 			{
 				try
 				{
-					var emotes_urls = await Get(EMOTES_LIST_URL, false);
-					if (emotes_urls.length < 150) { return setTimeout(InnerGetEmotesUrls, 500); }
-					if (emotes_urls == "ERROR") { return setTimeout(InnerGetEmotesUrls, 500); }
+					const emotes_urls = await Get(
+					{
+						url: "https://s1ye.github.io/GGExtraEmotes/list.txt",
+						from_cache: false,
+			 			max_attempts: 5,
+			 			retry_ms: 1000,
+						timeout_ms: 5000
+					});
 					var tmp = emotes_urls.split("\n");
 					for (var i = 0; i < tmp.length; i++)
 					{
@@ -173,33 +208,75 @@
 					}
 					resolve(tmp);
 				}
-				catch(e) { }
+				catch(e)
+				{
+					console.log(e);
+				}
 			}
 			await InnerGetEmotesUrls();
 		});
 	}
 
-	async function Get(url, cache)
+	async function Get({url, from_cache, max_attempts, retry_ms, timeout_ms} = {})
 	{
-		try
+		return await new Promise(async function(resolve, reject)
 		{
-			var response = "";
-			if (cache) { response = await fetch(url); }
-			else { response = await fetch(url, {cache: "no-cache"}); }
-			return await response.text();
-		}
-		catch(e) { return "ERROR"; }
-	};
+			var attempts_used = 0; 
+			GetWithRepeat();
+
+			async function GetWithRepeat()
+			{
+				try
+				{
+					attempts_used++;
+					return resolve(await InnerGet(
+					{
+						url: url,
+						from_cache: from_cache,
+						timeout_ms: timeout_ms
+					}));
+				}
+				catch(err)
+				{
+					console.log(err);
+					if (attempts_used >= max_attempts)
+					{
+						return reject(new Error("[GGExtraEmotes] Maximum number of attempts reached in Get('" + url + ", " + from_cache + ", " + max_attempts + ", " + retry_ms + ", " + timeout_ms + ")"));
+					}
+					setTimeout(GetWithRepeat, retry_ms);
+				}
+			}
+
+			async function InnerGet({url, from_cache, timeout_ms} = {})
+			{
+				var cache_setting = "default";
+				if (!from_cache) { cache_setting = "no-cache"; }
+				const controller = new AbortController();
+				const fetch_timeout = setTimeout(() => controller.abort(), timeout_ms);
+				return await fetch(url, { cache: cache_setting, signal: controller.signal}).then(async function(response)
+				{
+					if (response.status >= 400 && response.status < 600)
+					{
+						throw new Error("[GGExtraEmotes] InnerGet('" + url + "', " + from_cache + ", " + timeout_ms + ") return status: " + response.status);
+					}
+					return await response.text();
+				}).catch(function(err) { throw new Error(err); });
+			};
+		});
+	}
 
 	function Replace(str, a, b)
 	{
-		while (str.indexOf(a) > -1) { str = str.replace(a, b); }
-		return str;
+		var new_str = CopyStrByValue(str);
+		while (new_str.indexOf(a) > -1) { new_str = new_str.replace(a, b); }
+		return new_str;
 	}
 
-	function IsElement(obj)
+	function IsHTMLElement(obj)
 	{
 		try { return obj instanceof HTMLElement; }
 		catch(e) { return false; }
 	}
+
+	function CopyStrByValue(str) { return str.valueOf(); }
 })();
