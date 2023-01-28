d21db734ba3aa39912916729a26e63bb38899bcb
S1ye <54349457+S1ye@users.noreply.github.com>
2023-01-28 15:24:38 +0300

    Add files via upload

--- chrome_extension/js/inject.js
+++ chrome_extension/js/inject.js
@@ -1,3 +1,3 @@
-var script = document.createElement('script');
+const script = document.createElement('script');
 script.src = chrome.runtime.getURL("js/injected.js");
-(document.head||document.documentElement).appendChild(script);
+(document.head || document.documentElement).appendChild(script);
